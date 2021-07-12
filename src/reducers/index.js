4353bf0190d4ec25de4ed77e7adb8627c56167e6
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-07-12 18:50:46 -0400

    project

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
+import posts from './postsReducers';
+
+const rootReducer = combineReducers({
+  form,
+  posts
+});
+
+export default rootReducer;
